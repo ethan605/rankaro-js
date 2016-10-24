4ab9a206d4844509bd7dcda78344018e623fd36a
Ethan N <thanhnx.605@gmail.com>
2016-10-24 22:54:36 +0700

    Add babel-root-slash-import

---
+++ app/components/TestComp/index.js
@@ -0,0 +1,12 @@
+/**
+ * @providesModule RanKaro.Components.TestComp
+ * @flow
+ */
+
+import React from 'react';
+
+export default class TestComp extends React.Component {
+  render() {
+    return <h1>Hello World, it's TestComp</h1>;
+  }
+}
--- app/index.js
+++ app/index.js
@@ -5,8 +5,10 @@
 
 import React from 'react';
 
+import TestComp from 'app/components/TestComp';
+
 export default class App extends React.Component {
   render() {
-    return <h1>Hello World!</h1>;
+    return <TestComp />;
   }
 }
